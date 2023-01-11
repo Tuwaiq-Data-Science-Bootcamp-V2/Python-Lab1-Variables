f0ef5024165fc6d3d850dd98349a2b9d2bb915c9
Khalid AlShaib <115303105+Khalid-alsumali@users.noreply.github.com>
2023-01-11 13:44:09 +0300

    Add files via upload

---
+++ Day4-Lab1-Variables.py
@@ -0,0 +1,14 @@
+''' Define a variable that has a value of your favourite season.
+Define “x“ variable has an integer value and variable “y” has a float value.
+Assign One Value to Multiple Variables.
+Assign Many Values to Multiple Variables. '''
+
+#This shows my favourite season
+fav_season = 'winter'
+
+# x shows an integer value while y is a float
+x = 5
+y = 9.5
+
+#Multiple  Variables assigned to multiple variables
+a, b, c = 1, 2, 3
