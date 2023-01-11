20db21d43900714f567a15367532f83ab642cb7b
Munirah <mun_1998K@hotmail.com>
2023-01-11 13:18:14 +0300

    Lab1 Day4 Solution -Munirah Alsahly

---
+++ Day4_Lab1_MunirahAlsahly.py
@@ -0,0 +1,13 @@
+# 1. Define a variable that has a value of your favourite season
+my_favourite_season = "Winter"
+
+# 2. Define "x" variable has an integer value and variable "y" has a float value.
+x = 24
+y = 88.5
+
+# 3. Assign One Value to Multiple Variables 
+value_1 , value_2, value3 = 'one value'
+
+# 4. Assign Many Values to Multiple Variables
+var_1 , var_2 , var_3 = 10, 20, 30
+
