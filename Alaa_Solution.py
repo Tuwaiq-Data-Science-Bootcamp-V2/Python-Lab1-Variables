04897e61744a61df8ceb06a9bf4556336ea7b7c3
AlaaQadri <92630976+AlaaQadri@users.noreply.github.com>
2023-01-11 13:14:34 +0300

    Add files via upload

---
+++ Alaa_Solution.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def area_rect(width=int,hight=int):
+    a = width * hight
+    return a
+area_rect(3,4)
+
+
+# In[7]:
+
+
+my_fav_season = 'spring'
+x= 5
+y= 3.5
+a=b=c=3
+r , t , s = 5 , 7 , 10
+print(my_fav_season)
+print(x)
+print(y)
+print(a)
+print(b)
+print(c)
+print(r)
+print(t)
+print(s)
+
+
+# In[ ]:
+
+
+
+
