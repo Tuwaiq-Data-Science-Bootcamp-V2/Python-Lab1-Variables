6cd5e0c0870860d20ac428673943bf30d496e83d
Waleed <49982197+iledoh@users.noreply.github.com>
2023-01-11 13:19:33 +0300

    Waleed Mohammed Almutairi

---
+++ Day4-Lab1-Waleed Mohammed.py
@@ -0,0 +1,18 @@
+#Waleed Mohammed Almutairi
+
+
+#This is favourite season (string)
+season = 'winter' 
+
+#Variable x has integer value
+x = 24
+
+#Variable x has float value
+y = 33.8
+
+#One Value to Multiple Variables
+var1 = var2 = var3 = 'Waleed'
+
+#Many Values to Multiple Variables.
+v1 , v2 , v3 =(1,2,3)
+
