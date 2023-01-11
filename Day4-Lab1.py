dacb16dd6af2c476b4d78ebb2b58bfc1c1cf3e35
MoaathMog <moaathmog@gmail.com>
2023-01-11 13:16:12 +0300

    python lab has been added

---
+++ Day4-Lab1.py
@@ -0,0 +1,14 @@
+#String variable
+favourit_season = "winter"
+
+#integer variable x
+x = 5
+#float variable y
+y = 5.5
+
+#multiple variables have the same value
+a = b = c = 1
+
+#multiple assigning
+d, e, f = "d", "e", "f"
+
