cad192172dd77fdcaba33d4a71b1f0248c9a3b83
Lamamugren <122200553+Lamamugren@users.noreply.github.com>
2023-01-11 13:37:48 +0300

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+fav_season = "Winter"
+
+
+# In[4]:
+
+
+x = 5
+y =2.5
+
+
+# In[5]:
+
+
+a=b=c="winter"
+
+
+# In[8]:
+
+
+a, b, c = 1, 2.5, "winter"
+
+
+# In[ ]:
+
+
+
+
